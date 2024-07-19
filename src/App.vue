d6a2e00e4d45c32820ef79937ca5b7ca1c0a8837
Reymon <rahidalgob@uce.edu.ec>
2024-07-18 19:20:15 -0500

    Taller33 - Test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<insertar-estudiante/>
+<eliminar-estudiante/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import InsertarEstudiante from './pages/InsertarEstudiante.vue'
+import EliminarEstudiante from './pages/EliminarEstudiante.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InsertarEstudiante,
+    EliminarEstudiante
   }
 }
 </script>
---
+++ src/pages/EliminarEstudiante.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <h2>Eliminar Estudiante</h2>
+        <form>
+        <div class="form-group">
+            <label for="cedula">Cédula</label>
+            <input type="text" id="cedula" />
+        </div>
+        <div class="form-buttons">
+            <button type="submit">Eliminar</button>
+        </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
